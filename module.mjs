aeeb1d03b5acabf6013f58ba846611809d8131b8
Prajjwal sahu <70817186+Prajjwalsahu@users.noreply.github.com>
2022-03-02 14:33:50 +0530

    I learnt reactjs with harry sir

---
+++ module.mjs
@@ -0,0 +1,9 @@
+const a="Ram";
+const b="Shyam";
+const c="Rangeela";
+const d="Birju";
+export default d;
+export {a};
+export {b};
+export {c};
+export {d};
---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import xyz,{a,b,c,d} from './module.mjs'
+console.log(xyz);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
